6adf460c96d991340b808aea7cc72ff64b494484
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-06 03:13:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,10 +4,10 @@ require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
-  elsif int % 3 == 0
+  if int % 3 == 0
     "Fizz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
   end
