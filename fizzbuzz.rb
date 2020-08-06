7f0cda3b54e14448556757ddb5d828730d27ac9e
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-06 02:48:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,8 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+  puts  "Fizz" # Go fizz
+  end
 end
